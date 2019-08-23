ba9766c77d74a8647c1f867ff7cb22b1ab7bb9bc
Ruth Baker <ruthmoog@gmail.com>
2019-08-23 18:13:56 +0100

    small refactor

--- middle-char.js
+++ middle-char.js
@@ -1,10 +1,12 @@
 (function(exports) {
 
   function middleChar(string) {
+    var stringMiddle = string.length/2
+
     if (string.length % 2 !== 0) {
       return string[(string.length-1)/2];
     }
-    return string[string.length/2 - 1] + string[string.length/2];
+    return string[stringMiddle - 1] + string[stringMiddle];
   }
 
   exports.middleChar = middleChar;
